6bd6f33be2b0ee8bd1c0ae56b7aae63edddd68da
Ricky Pinder <pinderrr@outlook.com>
2017-12-19 21:41:41 +0000

    added first stage of prim algorithm, currently only does first edge

--- main.js
+++ main.js
@@ -63,6 +63,33 @@ function diagnosticText() {
 
 }
 
+function prim() {
+    var included_nodes = [nodes[0]]
+    var not_included_nodes = nodes.slice();
+    not_included_nodes.splice(0, 1);
+    var minimum_distance = distanceBetween(nodes[0], nodes[1]);
+    var next_edge = [0, 0];
+    edges = [];
+
+    for (var i = 0; i < included_nodes.length; i++) {
+        for (var j = 0; j < not_included_nodes.length; j++) {
+            if (distanceBetween(included_nodes[i], not_included_nodes[j]) < minimum_distance) {
+                next_edge = [i, j];
+            }
+        }
+    }
+
+    edges.push([included_nodes[next_edge[0]], not_included_nodes[next_edge[1]]]);
+    included_nodes.push(not_included_nodes[next_edge[1]]);
+    not_included_nodes.splice(next_edge[1], 1);
+}
+
+function drawRoute() {
+    for (var i = 0; i < edges.length; i++) {
+        drawLine(edges[i][0], edges[i][1]);
+    }
+}
+
 function printMousePos(event) {
     ctx.fillStyle = 'white';
     ctx.fillRect(0, 0, width, height);
@@ -76,7 +103,10 @@ function printMousePos(event) {
             posY > nodes[i].posY - 10 && posY < nodes[i].posY + 10) {
             nodes.splice(i, 1);
             drawNodes();
-            if (nodes.length > 1) drawLine(nodes[0], nodes[1]);
+            if (nodes.length > 1) {
+                prim();
+                drawRoute();
+            }
             if (diagnostic) diagnosticText();
             return;
         }
@@ -84,8 +114,11 @@ function printMousePos(event) {
 
     nodes.push(new Node(posX, posY));
     drawNodes();
-
-    if (nodes.length > 1) drawLine(nodes[0], nodes[1]);
+    if (nodes.length > 1) {
+        prim();
+        drawRoute();
+    }
+    
     if (diagnostic) diagnosticText();
 };
     
